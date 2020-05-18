a0540c159f81d42e87b8df90e38b3cad9b88a873
pawardcr <pawarin@thaidcr.com>
2020-05-18 14:54:08 +0700

    little change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'Q1.3';
+    title = 'app';
 
     columnDefs = [
         {headerName: 'Make', field: 'make' },
